e3b66979564840de169032da49ff140338bbf415
janvipravinhaipatel <158079903+janvipravinhaipatel@users.noreply.github.com>
2024-02-29 08:01:22 -0500

    First Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static class FizzBuzz {
+
+        public String play(int number) {
+          return "Fizz";
+        }
+    }
+}
